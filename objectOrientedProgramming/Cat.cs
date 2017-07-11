264455ed58befe0c4bf0b2a8326fe6a611817504
Your Name <you@example.com>
2017-07-11 10:07:28 -0400

    add cat class properties, constructors, etc.

--- objectOrientedProgramming/Cat.cs
+++ objectOrientedProgramming/Cat.cs
@@ -8,6 +8,52 @@ namespace objectOrientedProgramming
 {
     class Cat
     {
-        //Object blueprint for OOP. Named in Pascal case
+        //Object blueprint for OOP. Named using Pascal case.
+
+        //In OOp there are two types of states...
+        //fields
+        //properties - A sort of mix between a field and a method that allow controlled access
+
+        //...and behaviors
+        //Methods - repeatabale/reusable sections of code - Actions
+        //constructors - specialized methods that are used when instanting an object.
+
+        //Building a class
+
+        //Field
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //Property
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //Constructor
+        public Cat()
+        {
+            //Default constructors take no arguments or parameters.
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is this cat hungry?" + isHungry);
+        }
+
+
     }
 }
